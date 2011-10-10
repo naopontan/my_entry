f193adeb177c343572009e33ba48c0ade39a4304
nao taka <naopontan@gmail.com>
2011-10-10 16:50:10 +0900

    added entries table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :competition
+  has_many :entries
+  has_many :users, :through => :entries
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :entries
+  has_many :events, :through => :entries
 end
