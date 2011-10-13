dadcad04464e24ac35e8fba60d724ab8c486f431
nao taka <naopontan@gmail.com>
2011-10-13 22:10:01 +0900

    男子、女子それぞれのメソッドを追加してみた

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,5 @@
 class Competition < ActiveRecord::Base
   has_many :events
+  has_many :male_events, :class_name => "Event", :conditions => {:is_male => true}
+  has_many :female_events, :class_name => "Event", :conditions => {:is_male => false}
 end
