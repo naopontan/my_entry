61cd3cb30ebf3865ae0613ace74ca61df2af3c72
nao taka <naopontan@gmail.com>
2011-10-13 11:28:16 +0900

    申込機能の実装の為。まずは表示から。 delegate や default_scope を使ってみた

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,6 @@
 class Entry < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
+
+  delegate :competition, :to => :event
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :entries
   has_many :events, :through => :entries
+
+  default_scope :order => 'name ASC'
 end
